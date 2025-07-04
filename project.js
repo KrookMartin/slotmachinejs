221f0c6e7850d636c155438e3d7017e266aa8ad5
KrookMartin <krookmartin@yahoo.com>
2025-07-04 12:29:57 +0200

    Add completed printrow func and getWinnings

--- project.js
+++ project.js
@@ -75,20 +75,50 @@ const spin = () => {
 
         }
     }
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++){
         const reelSymbols = [...symbols];
+        reels.push([]);
         for (let j = 0; j < ROWS; j++) {
-            const selectedSymbol = reelSymbols[]
+            const randomIdex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIdex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIdex, 1);
         }
     }
-    
+    return reels;
 };
 
-spin();
+const transpose = (reels) => {
+    const rows = []
 
+    for (let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows; 
+}
+
+const printRows = (rows) => {
+    for (const row of rows){
+        let rowString ="";
+        for (const [i , symbol] of row.entries()){
+            rowString += symbol 
+            if(i != row.length -1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString);
+    }
+}
 
 let  balance = deposit();
 const numberOfLines = getNumberOfLine();
-const bet = getBet(balance,numberOfLines);
- 
+const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
+//console.log(reels);
+//console.log(rows);
